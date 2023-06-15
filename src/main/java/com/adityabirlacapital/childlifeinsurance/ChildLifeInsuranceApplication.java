80c810ddd6abab499f20fda253fdb78316333244
gs-3112 <amol.shinde@gslab.com>
2023-06-15 19:06:37 +0530

    First commit

---
+++ src/main/java/com/adityabirlacapital/childlifeinsurance/ChildLifeInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.adityabirlacapital.childlifeinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildLifeInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildLifeInsuranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adityabirlacapital/childlifeinsurance/ChildLifeInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adityabirlacapital.childlifeinsurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildLifeInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
